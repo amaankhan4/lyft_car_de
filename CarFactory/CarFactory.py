3dd8c0bcd39db7dd1845f82efd8edac9a39b1bc2
amaankhan4 <77272514+amaankhan4@users.noreply.github.com>
2023-05-21 21:18:03 +0530

    Update CarFactory.py

--- CarFactory/CarFactory.py
+++ CarFactory/CarFactory.py
@@ -1,13 +1,30 @@
-import engine
-import Battery
+from engine.CapuletEngine import CapuletEngine
+from engine.SternmanEngine import SternmanEngine
+from engine.WilloughbyEngine import WilloughbyEngine
+from Battery.Nubbing import Nubbing
+from Battery.Spindler import Spindler
+from car import Car
 class CarFactory:
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
-        pass
+        engine=CapuletEngine(current_mileage, last_service_mileage)
+        battery=Spindler(current_date, last_service_date)
+        return car(engine,battery)
+
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
-        pass
+        engine=WillooughbyEngine(current_mileage, last_service_mileage)
+        battery=Spindler(current_date, last_service_date)
+        return car(engine,battery)
+
     def create_palindrome(current_date, last_service_date, warning_light_on):
-        pass
+        engine=SternmanEngine(warning_light_on)
+        battery=Spindler(current_date, last_service_date)
+        return car(engine,battery)
+
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
-        pass
+        engine=WillooughbyEngine(current_mileage, last_service_mileage)
+        battery=Nubbing(current_date, last_service_date)
+        return car(engine,battery)
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
-        pass
+        engine=CapuletEngine(current_mileage, last_service_mileage)
+        battery=Nubbing(current_date, last_service_date)
+        return car(engine,battery)
