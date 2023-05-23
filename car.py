4e537fc06b2fb7c8929407ae575521c638c1fcaf
amaankhan4 <77272514+amaankhan4@users.noreply.github.com>
2023-05-23 18:58:08 +0530

    Update car.py

--- car.py
+++ car.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery,tire):
         self.engine = engine
         self.battery = battery
+        self.tire=tire
 
     @abstractmethod
     def needs_service(self):
