d888e70475be0148bcf8213e244bfab713b11a79
amaankhan4 <77272514+amaankhan4@users.noreply.github.com>
2023-05-23 18:57:04 +0530

    Create tire.py

---
+++ tire.py
@@ -0,0 +1,13 @@
+import numpy
+class tire:
+  def carrigen_tire(self,tire_condition_arr):
+    if(max(tire_condition_arr)>=0.9)
+        return true
+    else
+        return false
+
+  def  Octoprime_tire(self,tire_condition_arr):
+    if(sum(tire_condition_arr)>=3)
+        return true
+    else
+        return false
