44aac0e24ea84830ead7ee35b62296c759cce29b
amaankhan4 <77272514+amaankhan4@users.noreply.github.com>
2023-05-23 19:11:32 +0530

    Create test_tire.py

---
+++ test/test_tire.py
@@ -0,0 +1,14 @@
+from car import car
+import unittest
+class test_tire(unittest.TestCase):
+    def carrigen_tireNotBeServiced(self):
+        self.assertFalse(carrigen_tire(tire_condition_arr))
+        
+     def carrigen_tireToBeServiced(self):
+        self.assertTrue(carrigen_tire(tire_condition_arr))
+        
+     def octoprime_tireNotBeServiced(self):
+        self.assertFalse(Octoprime_tire(tire_condition_arr))
+        
+     def octoprime_tireToBeServiced(self):
+        self.assertTrue(Octoprime_tire(tire_condition_arr))  
